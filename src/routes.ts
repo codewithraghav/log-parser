47e197bf3566728dd85929041596a62c5be03f55
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2025-04-21 20:13:59 +0530

    update routes signture with actual function call

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
-
+import { parseLogFile, parseUniqueViews } from './parser';
 const router = Router();
 
 router.get('/get-most-page-views', async (req, res) => {
+
+    try {
+        const logEntries = await parseLogFile();
+        res.json(logEntries);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to parse log file.' });
+      }
   
 });
 
 router.get('/get-unique-views', async (req, res) => {
   
+    try {
+        const uniqueViews = await parseUniqueViews();
+        res.json(uniqueViews);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to parse unique views.' });
+      }
 });
 
 export default router;
