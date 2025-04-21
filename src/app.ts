ebbb2ffa35b6b39996c8ae6c2faec964456d11cb
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2025-04-21 20:41:24 +0530

    Update default endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const PORT = 3009;
+const PORT = 3011;
 
-app.use('/', routes);
+app.use('/get-most-page-views', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
