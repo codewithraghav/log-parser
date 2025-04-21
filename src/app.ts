9e8e1f7abd09c7eacfa433218fc082a2ea516dc6
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2025-04-21 20:04:11 +0530

    use routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-
+import routes from './routes';
 
 const app = express();
 const PORT = 3008;
 
-
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
