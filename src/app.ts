cee01bfc1288cc4ec39c82df2313820d14e1b7e7
Raghavendra Kulkarni <raghavendrakulkarni@Raghavendras-M1-Pro.local>
2025-04-21 20:45:59 +0530

    redirect to default parse-log endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const PORT = 3011;
+const PORT = 3012;
 
-app.use('/get-most-page-views', routes);
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+  res.redirect('/parse-log');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
